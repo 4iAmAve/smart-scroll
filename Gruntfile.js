a9eff8f845daf747fe571f0a75f0c80bb227a2e5
Ave <me@aves.site>
2015-09-16 13:22:21 +0200

    added License file + adjusted grunt package.json + added intial files

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,88 @@ module.exports = function(grunt) {
   // config
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      options: {
+        interrupt: true,
+        livereload: true
+      }
+    },
+
+    shell: {
+      install: {
+        command: 'sh dev/install-remote.sh '+ grunt.option('package'),
+        options: grunt.option('commandline-options')
+      },
+
+      install_vm: {
+        command: 'sh dev/install-vm.sh '+ grunt.option('package'),
+        options: grunt.option('commandline-options')
+      },
+
+      install_csvm: {
+        command: 'sh dev/install-csvm.sh '+ grunt.option('package'),
+        options: grunt.option('commandline-options')
+      }
+    },
+
+    todo: {
+      options: {
+        marks: [
+          {
+            name: "FIX",
+            pattern: /FIXME/,
+            color: "red"
+          },
+          {
+            name: "TODO",
+            pattern: /TODO/,
+            color: "yellow"
+          },
+          {
+            name: "NOTE",
+            pattern: /NOTE/,
+            color: "blue"
+          }
+        ],
+        file: "refactor-report.md"
+      },
+      src: [
+        'src/ION/**/*', 'assets/**/*', 'templates/**/*'
+      ]
+    }
   });
+
+  grunt.registerTask('development', '', [
+      'clean:build',
+      'sudo_subcomponents:build',
+      'jade:components',
+      'jade:index',
+      'less:compile',
+      'uglify:vendor',
+      'uglify:app',
+      'uglify:templates',
+      'copy:fonts',
+      'copy:img',
+      'copy:index'
+  ]);
+
+  grunt.registerTask('build', '', [
+      'clean:build',
+      'sudo_subcomponents:build',
+      'jade:components',
+      'jade:index',
+      'less:compile',
+      'uglify:vendor',
+      'uglify:app',
+      'uglify:templates',
+      'copy:fonts',
+      'copy:img',
+      'copy:index'
+  ]);
+
+  grunt.registerTask('deploy', '', [
+  ]);
 
   grunt.loadTasks(grunt.config('pkg.project.directories.build-tasks'));
 };
